50ef01bd59be5629fff4c9eefe5c3596a768392e
Svyatoslav Sharov <sharov.so@phystech.edu>
2023-10-01 15:03:47 +0300

    New constant TYPE_HORSE added

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import  java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        String TYPE_HORSE = "horse";
         Scanner input = new Scanner(System.in);
         Animal animal = null;
 
@@ -12,7 +13,7 @@ public class Main {
 
 
 
-        if (a.equals("horse")) {
+        if (a.equals(TYPE_HORSE)) {
             animal = new Horse("land", "herbivorous");
         } else if (a.equals("tiger")) {
             animal = new Tiger("land", "predator", "beef");
@@ -43,4 +44,5 @@ public class Main {
             return;
         }
     }
+
 }
