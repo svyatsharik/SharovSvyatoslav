0bc8399d377f7539e7fed14eaaf08c4ea0fe6c30
Svyatoslav Sharov <sharov.so@phystech.edu>
2023-10-07 12:51:51 +0300

    Написал все классы

---
+++ src/main/java/ru/mts/homework2/Dolphin.java
@@ -0,0 +1,13 @@
+package ru.mts.homework2;
+
+public class Dolphin implements WaterAnimal, PredatoryAnimal {
+    @Override
+    public void printPredatoryAnimal() {
+        System.out.println("Дельфин - хищник");
+    }
+
+    @Override
+    public void printWaterAnimal() {
+        System.out.println("Дельфин - водоплавающий");
+    }
+}
---
+++ src/main/java/ru/mts/homework2/Horse.java
@@ -0,0 +1,13 @@
+package ru.mts.homework2;
+
+public class Horse implements LandAnimal, HerbivorousAnimal {
+    @Override
+    public void printHerbivorousAnimal() {
+        System.out.println("Лошадь - травоядная");
+    }
+
+    @Override
+    public void printLangAnimal() {
+        System.out.println("Лошадь - сухопутная");
+    }
+}
