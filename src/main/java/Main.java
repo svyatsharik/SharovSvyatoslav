d168a3b8c1393b707ffb57899c845bed704a1d22
Svyatoslav Sharov <sharov.so@phystech.edu>
2023-10-09 21:41:03 +0300

    Прописана реализация в Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,34 @@
-import main.java.ru.mts.homework.animal.Dolphin;
-import main.java.ru.mts.homework.animal.Eagle;
-import main.java.ru.mts.homework.animal.*;
+
+import ru.mts.homework.sort.Sorts;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Eagle eagle = new Eagle();
-        Dolphin dolphin = new Dolphin();
+        Scanner input = new Scanner(System.in);
+        int n, k;
 
-        eagle.printFlyingAnimal();
-        dolphin.printPredatoryAnimal();
+        System.out.println("Введите размер массива, а затем введите сам массив:");
+
+        n = input.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = input.nextInt();
+        }
+
+        System.out.println("Выберите тип сортировки: 1 - сортировка пузырьком, иначе - сортировка слиянием");
+        k = input.nextInt();
+
+        if (k == 1) {
+            Sorts.bubbleSort(arr);
+        } else {
+            Sorts.basicSort(arr);
+        }
+
+        System.out.println("Отсортированный массив:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
     }
 }
