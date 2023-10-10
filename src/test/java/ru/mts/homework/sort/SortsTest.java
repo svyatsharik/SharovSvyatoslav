7a092f6812c332f999f38f5d3bb3c57ed2316359
Svyatoslav Sharov <sharov.so@phystech.edu>
2023-10-10 13:53:52 +0300

    Прописаны тесты и настроен Jacoco

--- src/test/java/ru/mts/homework/sort/SortsTest.java
+++ src/test/java/ru/mts/homework/sort/SortsTest.java
@@ -6,7 +6,7 @@ class SortsTest {
 
     @org.junit.jupiter.api.Test
     void bubbleSort() {
-        assertEquals(new int[] {6,3,5,1,2,4,7,9,8}, new int[] {1,2,3,4,5,6,7,8,9});
+        assertEquals(Sorts.basicSort(new int[] {6,3,5,1,2,4,7,9,8}), new int[] {1,2,3,4,5,6,7,8,9});
     }
 
     @org.junit.jupiter.api.Test
