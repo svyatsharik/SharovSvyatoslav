33eff9a395c6e7ecb9d3fb491d5838dd02301db7
Svyatoslav Sharov <sharov.so@phystech.edu>
2023-10-13 19:36:38 +0300

    Проверил работоспособность тестов

--- src/test/java/ru/mts/homework/sort/SortsTest.java
+++ src/test/java/ru/mts/homework/sort/SortsTest.java
@@ -12,6 +12,7 @@ class SortsTest {
         assertTrue(Arrays.equals(new int[] {1,2,3,4,5,6,7,8,9}, Sorts.bubbleSort(new int[] {6,3,5,1,2,4,7,9,8})));
     }
 
+
     @Test
     void basicSort() {
         assertTrue(Arrays.equals(new int[] {1,2,3,4,5,6,7,8,9}, Sorts.basicSort(new int[] {6,3,5,1,2,4,7,9,8})));
