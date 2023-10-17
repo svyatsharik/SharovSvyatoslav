2d754131702fdc342e5193b77cd34dc86bb12e54
Svyatoslav Sharov <sharov.so@phystech.edu>
2023-10-17 13:59:02 +0300

    Почти доделал

---
+++ src/test/java/BubbleSortTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+import ru.mts.homework.sort.BubbleSort;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BubbleSortTest {
+    @Test
+    void bubbleSort() {
+        assertTrue(Arrays.equals(new int[] {1,2,3,4,5,6,7,8,9}, BubbleSort.bubbleSort(new int[] {6,3,5,1,2,4,7,9,8})));
+    }
+
+}
